c8100b1571fd3910567b70fbbc5f6e7a9a36d085
DwarfPorter <mrvlmor@gmail.com>
2021-07-22 20:54:07 +0300

    Сравнение классов Java и Kotlin

---
+++ app/src/main/java/ru/geekbrains/javatokotlin/data/Weather.kt
@@ -0,0 +1,3 @@
+package ru.geekbrains.javatokotlin.data
+
+data class Weather(var town: String, var temperature: Int)
---
+++ app/src/main/java/ru/geekbrains/javatokotlin/data/WeatherJava.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.javatokotlin.data;
+
+public class WeatherJava {
+    private String town;
+    private int temperature;
+
+    public WeatherJava(String town, int temperature) {
+        this.setTown(town);
+        this.setTemperature(temperature);
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+}
+
