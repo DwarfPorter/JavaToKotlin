fc68ebd5f7b2a96345e27857818634b0690ffd71
DwarfPorter <mrvlmor@gmail.com>
2021-07-22 21:27:32 +0300

    инициализируем recyclerview в активити

--- app/src/main/java/ru/geekbrains/javatokotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/javatokotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package ru.geekbrains.javatokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import ru.geekbrains.javatokotlin.R
+import ru.geekbrains.javatokotlin.repository.RepositorySingle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerWeather = findViewById<RecyclerView>(R.id.recyclerWeather)
+        val adapter = WeatherAdapter(RepositorySingle)
+        recyclerWeather.adapter = adapter
     }
 }
