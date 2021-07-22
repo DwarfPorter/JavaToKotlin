681a9a3a792c35f4336589e9218ae65135aed39b
DwarfPorter <mrvlmor@gmail.com>
2021-07-22 21:24:38 +0300

    создали адаптер для recyclerView

---
+++ app/src/main/java/ru/geekbrains/javatokotlin/WeatherAdapter.kt
@@ -0,0 +1,41 @@
+package ru.geekbrains.javatokotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import ru.geekbrains.javatokotlin.data.Weather
+import ru.geekbrains.javatokotlin.repository.IRepository
+
+class WeatherAdapter(private val repository: IRepository) :
+    RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+    class WeatherViewHolder(val item: View) : RecyclerView.ViewHolder(item) {
+        private val city: TextView
+
+        private val temperature: TextView
+
+        init {
+            city = item.findViewById(R.id.textCity)
+            temperature = item.findViewById(R.id.textTemp)
+        }
+
+        fun bind(weather: Weather){
+            city.text = weather.town
+            temperature.text = weather.temperature.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        return WeatherViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
+        holder.bind(repository.getWeathers()[position])
+    }
+
+    override fun getItemCount(): Int {
+        return repository.getWeathers().size
+    }
+}
